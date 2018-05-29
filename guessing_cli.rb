d010f3ae650cdb9069551b5343c7b80b6c059d2e
Jigar Polra <jigarpolra@live.co.uk>
2018-05-29 14:02:31 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,13 +2,15 @@
 def run_guessing_game
   exit_program = false
 
-  while(!exit) do
+  while(!exit_program) do
     puts "Guess a number between 1 and 6."
     computer_guess = rand(1..6).to_i
     userinput = (gets.chomp)
-    if(userinput.lowercase == "exit")
-      exit_program = true
-    end
+
+    if(userinput == "exit") then
+       exit_program = true
+     end
+
     if(userinput.to_i == computer_guess)
       puts "You guessed the correct number!"
     else
